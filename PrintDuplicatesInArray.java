a6c27f3dea273dd9bea9d0410cc62d46e47d8747
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-05-03 03:24:03 +0530

    Add files via upload

---
+++ PrintDuplicatesInArray.java
@@ -0,0 +1,18 @@
+package week3.day2;
+
+public class PrintDuplicatesInArray {
+	public static void main(String[] args) {
+		
+		//To find the duplicates in the array
+
+		int[] arr = { 14, 12, 13, 11, 15, 14, 18, 16, 17, 19, 18, 17, 20 };
+		System.out.println("Duplicates in the array is :");
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j])
+					System.out.println(arr[j] + " ");
+			}
+		}
+	}
+}
---
+++ RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package week3.day2;
+
+public class RemoveDuplicates {
+
+	public static void main(String args[]) {
+		
+		//To remove the duplicate words in a string
+		
+		String text = "We learn java basics as part of java sessions in java week1";
+		String[] words = text.split(" ");
+
+		for (int i = 0; i < words.length; i++) {
+			for (int j = 0; j < words.length; j++) {
+				if (words[i].equals(words[j])) {
+					if (i != j)
+						words[i] = "";
+				}
+			}
+		}
+
+		System.out.println("Removing Duplicate words:\n");
+		for (int i = 0; i < words.length; i++) {
+
+			if (words[i] != "") {
+				System.out.print(words[i] + " ");
+			}
+		}
+	}
+}
