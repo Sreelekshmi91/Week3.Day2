95885b6709148c5815ef165176cd54e0a23412ca
Sreelekshmi91 <102054755+Sreelekshmi91@users.noreply.github.com>
2022-05-05 02:04:00 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package week3.day2;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+
+	public static void main(String args[]) {
+
+		// To remove the duplicate words in a string
+
+		String text = "We learn java basics as part of java sessions in java week1";
+		String[] split = text.split(" ");
+		Set<String> set = new LinkedHashSet<String>();
+		for (String string : split) {
+			set.add(string);
+		}
+		System.out.println(set);
+	}
+}
